bc3cce0e3f573e104dbdbf2178581255707e3f92
Faiz <mohdfaiz.1842@gmail.com>
2023-06-18 23:15:42 +0530

    push code

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Header Scroll
+
+// Nav hide
+
+
+// Swiper slider
+var swiper = new Swiper(".mySwiper", {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        },
+        autoplay: {
+            delay: 3500,
+        },
+});
+
+// Counter design
+
+
+// our partner
