ba0478a0f1dd07834de015c9a448d84dcf07a01a
Faiz <mohdfaiz.1842@gmail.com>
2023-06-20 13:32:39 +0530

    add all files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 // Header Scroll
-
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
 // Nav hide
-
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a) {
+    a.addEventListener("click", function() {
+        navCollapse.classList.remove("show");
+    })
+});
 
 // Swiper slider
 var swiper = new Swiper(".mySwiper", {
