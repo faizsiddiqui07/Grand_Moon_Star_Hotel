9fdf3de0a7c9d904b6125a0788c92187948b3022
Faiz <mohdfaiz.1842@gmail.com>
2023-07-02 13:10:18 +0530

    add more code

--- js/main.js
+++ js/main.js
@@ -45,10 +45,32 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }, step);   
     }
-    counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
+    counter("count1", 0, 387, 3000);
+    counter("count2", 100, 1786, 2500);
     counter("count3", 0, 1440, 3000);
-    counter("count4", 0, 7110, 3000);
+    counter("count4", 0, 2110, 3000);
 });
 
 // our partner
+var swiper = new Swiper(".our-partner", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        '991': {
+            slidesPerView: 5,
+            spaceBetween: 10,
+    },
+        '767': {
+            slidesPerView: 3,
+            spaceBetween: 10,
+    },
+        '320': {
+            slidesPerView: 2,
+            spaceBetween: 8,
+    },
+}
+});
